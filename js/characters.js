25f57d06d1ebe8362b9bc83d019dfdfa3cf8d976
juwanpetty <jchpetty@gmail.com>
2018-08-22 14:40:47 -0400

    Simplify character states

--- js/characters.js
+++ js/characters.js
@@ -2,9 +2,9 @@ function Characters(name) {
     // Player Stats
     this.name = name;
     this.health = 100;
-    this.speed = 2.5;
+    this.speed = 4;
 
-    this.image = new Sprites('img/rogueplayer.png', 8, 8, 32, 64);
+    this.image = new Sprites('img/rogueplayer.png', 10, 10, 32, 64);
 
     // Player State
     this.states = {
@@ -19,18 +19,14 @@ function Characters(name) {
     this.spriteIndex = this.states.idle;
     
     // Player Position and Depth
-    this.x = 0;
-    this.y = 0;
+    this.x = 64 * 3;
+    this.y = 64 * 4;
+
     this.width = this.image.desiredSize;
     this.height = this.image.desiredSize;
 
     // Player Aiming
     this.aimAngle = 0;
-
-    // Player Animation
-    this.spriteAnimationCounter = 0;
-    this.attackAnim = 0;
-    this.isAttacking = false;
     
     // Keyboard Controls
     this.pressingDown = false;
@@ -39,71 +35,29 @@ function Characters(name) {
     this.pressingRight = false;
 }
 
-Characters.prototype.update = function(map) {
-    this.updatePosition(map);
+Characters.prototype.update = function() {
+    this.updatePosition();
     this.draw();
-    this.spriteAnimationCounter += 0.15;
-
-    if (this.state === this.states.attack || (this.state === this.states.death && this.attackAnim <= 5)) {
-        this.attackAnim += 0.15;
-    } else if (this.state === this.states.death && this.attackAnim >= 5) {
-        this.attackAnim = 5;
-    } else {
-        this.attackAnim = 0;
-    }
 }
 
-Characters.prototype.updatePosition = function(map) {
-    // if player side touches collision tile 
-    // don't go in direction of collision tile
-    let playerLeft = (this.x - this.width / 2);
-    let playerRight = (this.x + this.width / 2);
-    let playerTop = (this.y - this.height / 2);
-    let playerBottom = (this.y + this.height / 2);
+Characters.prototype.updatePosition = function() {
 
-    const collisionPlayerTop = map.isSolidTileAtXY(player.x, playerTop);
-    const collisionPlayerRight = map.isSolidTileAtXY(playerRight, player.y);
-    const collisionPlayerBottom = map.isSolidTileAtXY(player.x, playerBottom);
-    const collisionPlayerLeft = map.isSolidTileAtXY(playerLeft, player.y);
+    if (this.pressingUp)
+        this.y -= this.speed; 
+            
+    if (this.pressingRight)
+        this.x += this.speed;
 
-    if (this.health !== 0) {
-        if (collisionPlayerTop) {
-            // this.y += this.speed; 
-        } else {
-            if (this.pressingUp) {
-                this.y -= this.speed; 
-            }
-        }
+    if (this.pressingDown)
+        this.y += this.speed; 
 
-        if (collisionPlayerRight) {
-            // this.x -= this.speed;
-        } else {
-            if (this.pressingRight) {
-                this.x += this.speed;
-            }
-        }
-
-        if (collisionPlayerBottom) {
-            // this.y -= this.speed;
-        } else {
-            if (this.pressingDown) {
-                this.y += this.speed;  
-            }
-        }
-
-        if (collisionPlayerLeft) {
-            // this.x += this.speed;
-        } else {
-            if (this.pressingLeft) {
-                this.x -= this.speed;  
-            }
-        }
-    }
+    if (this.pressingLeft)
+        this.x -= this.speed;  
     
-    if (this.x < this.width / 2) this.x = this.width / 2;
-    if (this.x > canvasWidth - this.width / 2) this.x = canvasWidth - this.width / 2;
-    if (this.y < this.height / 2) this.y = this.height / 2;
-    if (this.y > canvasHeight - this.height / 2) this.y = canvasHeight - this.height / 2;
+    // if (this.x < this.width / 2) this.x = this.width / 2;
+    // if (this.x > canvasWidth - this.width / 2) this.x = canvasWidth - this.width / 2;
+    // if (this.y < this.height / 2) this.y = this.height / 2;
+    // if (this.y > canvasHeight - this.height / 2) this.y = canvasHeight - this.height / 2;
 }   
 
 Characters.prototype.draw = function() {
@@ -122,8 +76,24 @@ Characters.prototype.draw = function() {
     }
 
     context.beginPath();
-    context.rect(this.x - (this.width / 2), this.y - (this.height / 2), this.width, this.height);
+    context.rect(
+        Math.round(this.x - camera.x + canvasWidth / 2 - camera.width / 2 - this.width / 2), 
+        Math.round(this.y - camera.y + canvasHeight / 2 - camera.height / 2 - this.width / 2), 
+        this.width, 
+        this.height
+    );
     context.stroke();
+
+    document.onmousemove = event => {
+        let mouseX = event.clientX;
+        let mouseY = event.clientY;
+    
+        mouseX -= Math.round(player.x - camera.x + canvasWidth / 2 - camera.width / 2 - player.width / 2);
+        mouseY -= Math.round(player.y - camera.y + canvasHeight / 2 - camera.height / 2 - player.height / 2);
+    
+        // console.log(Math.atan2(mouseY, mouseX) / Math.PI * 180);
+        player.aimAngle = Math.atan2(mouseY, mouseX) / Math.PI * 180;
+    }
 
     context.restore();
 }
@@ -131,15 +101,11 @@ Characters.prototype.draw = function() {
 Characters.prototype.idle = function() {
     this.spriteIndex = this.states.idle;
 
-    const x = this.x - this.width / 2;
-    const y = this.y - this.height / 2;
-
-    let spriteAnimation = Math.floor(this.spriteAnimationCounter) % 8;
-
-    playerSprite.play(spriteAnimation, x, y);
-
-    // context.drawImage(image, spriteAnimation * cropWidth, this.spriteIndex * cropHeight, cropWidth, cropHeight,
-    //     x, y, drawWidth, drawHeight);
+    this.image.play(
+        Math.round(this.x - camera.x + canvasWidth / 2 - camera.width / 2 - this.width / 2), 
+        Math.round(this.y - camera.y + canvasHeight / 2 - camera.height / 2 - this.width / 2), 
+        [0, 1, 2, 3, 4, 5, 6, 7]
+    );
 
     if (this.pressingRight || this.pressingLeft || this.pressingDown || this.pressingUp) {
         this.state = this.states.wander;
@@ -158,29 +124,28 @@ Characters.prototype.idle = function() {
 }
 
 Characters.prototype.wander = function() {
-    this.spriteIndex = 1;
+    let frames = [];
 
-    const x = this.x - this.width / 2;
-    const y = this.y - this.height / 2;
+    if (this.pressingUp) {
+        frames = [20, 21, 22, 23, 24, 25];
+    } else if (this.pressingLeft) {
+        frames = [80, 81, 82, 83, 84, 85];
+    } 
+    else {
+        frames = [10, 11, 12, 13, 14, 15];
+    }
 
-    const image = this.image.image;
-    const cropWidth = this.image.size;
-    const cropHeight = this.image.size;
-    const drawWidth = this.width;
-    const drawHeight = this.height;
-
-    let mod = 6;
-    let spriteAnimation = Math.floor(this.spriteAnimationCounter) % mod;
-
-    context.drawImage(image, spriteAnimation * cropWidth, this.spriteIndex * cropHeight, cropWidth, cropHeight,
-        x, y, drawWidth, drawHeight);
+    this.image.play(
+        Math.round(this.x - camera.x + canvasWidth / 2 - camera.width / 2 - this.width / 2), 
+        Math.round(this.y - camera.y + canvasHeight / 2 - camera.height / 2 - this.width / 2),
+        frames
+    );
 
     if (!this.pressingRight && !this.pressingLeft && !this.pressingDown && !this.pressingUp) {
         this.state = this.states.idle;
     }
 }
 
-// this will be moved to enemy
 Characters.prototype.alert = function() {
     console.log('Alert...');
 
@@ -189,51 +154,46 @@ Characters.prototype.alert = function() {
 
 Characters.prototype.attack = function() {
     this.spriteIndex = this.states.attack;
+    let frames = [];
 
-    const x = this.x - this.width / 2;
-    const y = this.y - this.height / 2;
+    if (this.aimAngle < 0) {
+        this.aimAngle = 360 + this.aimAngle;
+    }
 
-    const image = this.image.image;
-    const cropWidth = this.image.size;
-    const cropHeight = this.image.size;
-    const drawWidth = this.width;
-    const drawHeight = this.height;
+    if (this.aimAngle >= 45 && this.aimAngle <= 135) { // down
+        frames = [50, 51, 52, 53, 54, 55];
 
-    const mod = 5; // number of indexes
-    let spriteAnimation = Math.floor(this.attackAnim) % mod;
+    } else if (this.aimAngle >= 135 && this.aimAngle <= 225) { // left
+        frames = [90, 91, 92, 93, 94];
+
+    } else if (this.aimAngle >= 225 && this.aimAngle <= 315) { // up
+        frames = [40, 41, 42, 43, 44, 45]; 
+
+    } else {
+        frames = [30, 31, 32, 33, 34];
+    }
+
+    this.image.play(
+        Math.round(this.x - camera.x + canvasWidth / 2 - camera.width / 2 - this.width / 2), 
+        Math.round(this.y - camera.y + canvasHeight / 2 - camera.height / 2 - this.width / 2), 
+        frames
+    );
 
     document.onmouseup = () => {
         this.isAttacking = false;
     }
     
-    if (spriteAnimation === (mod - 1) && !this.isAttacking) {
+    if (!this.isAttacking) {
         this.state = this.states.idle;
     }
-    
-    context.drawImage(image, spriteAnimation * cropWidth, this.spriteIndex * cropHeight, cropWidth, cropHeight,
-        x, y, drawWidth, drawHeight);
 }
 
 Characters.prototype.death = function() {
-    const x = this.x - this.width / 2;
-    const y = this.y - this.height / 2;
-
-    const image = this.image.image;
-    const cropWidth = this.image.size;
-    const cropHeight = this.image.size;
-    const drawWidth = this.width;
-    const drawHeight = this.height;
-
-    const mod = 6;
-    let spriteAnimation = Math.floor(this.attackAnim) % mod;
-    
-    if (spriteAnimation === (mod - 1) && this.health === 0) {
-        context.drawImage(image, (mod - 1) * cropWidth, 7 * cropHeight, cropWidth, cropHeight,
-            x, y, drawWidth, drawHeight);
-    } else {
-        context.drawImage(image, spriteAnimation * cropWidth, 7 * cropHeight, cropWidth, cropHeight,
-            x, y, drawWidth, drawHeight);
-    }
+    this.image.play(
+        Math.round(this.x - camera.x + canvasWidth / 2 - camera.width / 2 - this.width / 2), 
+        Math.round(this.y - camera.y + canvasHeight / 2 - camera.height / 2 - this.width / 2), 
+        [70, 71, 72, 73, 74, 75]
+    );
 
     if (player.health > 0) {
         this.state = this.states.idle;
